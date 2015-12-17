85da84f897474cba39f2115187a3cc039371c3b5
kwansima kwakye <kwansimakwakye@gmail.com>
2015-12-17 10:41:45 -0800

    this allows an administrator to search a nurse

--- Add Nurse/addnurse.php
+++ Add Nurse/addnurse.php
@@ -1,19 +1,17 @@
 <?php
-
-/**
-*@author Adjoa Kwakye
-*this page contains all functions
-**/
-	include_once("adb.php");
-class nurse extends adb {
-	
-	
-
-	function nurse(){
-	
-
-	}
 	/**
+*@author Adjoa Kwakye
+*@version 1.0.2
+*this page contains all functions for a nurse
+**/
+include_once("adb.php");
+    class nurse extends adb
+    {
+        function nurse()
+        {
+        }
+
+        /**
 	*@method boolean add_nurse() This Adds a Nurse
 	*@param String name name of nurse
 	*@param String account whether the user is an administrator or nurse
@@ -21,55 +19,37 @@ class nurse extends adb {
 	*@param String password the password which will be given to a nurse
 	*@return return bool
 	**/
-	
-	function add_nurse($name,$account,$email,$password)
-		{
-
-		$str_query = "INSERT into h_account set h_fullname='$name',
+        function add_nurse($name,$account,$email,$password)
+        {
+            $str_query = "INSERT into h_account set h_fullname='$name',
 		                                      h_account_type='$account',
 		                                      h_email='$email',
 		                                      h_password='$password' ";
-											   
-											   
-											 
+            return $this->query($str_query);
+        }
 
-		return $this->query($str_query);
-	}
 
-	/**
+        /**
 	*@method boolean delete_nurse() Deletes a Nurse
 	*@param Int id the id of a nurse
 	*@return return bool
 	**/
+        function delete_nurse($id)
+        {
+            $str_query = "DELETE from h_account where h_account_id ='$id'";
+            return $this->query($str_query);
+        }
+        
 
-	function delete_nurse($id){
-		$str_query = "DELETE from h_account where h_account_id ='$id'";
-		return $this->query($str_query);
-
-	}
-	/**
+        /**
 	*@method boolean search_nurse() Searches a Nurse
 	*@param String name the name of a nurse
 	*@return return bool
 	**/
+        function search_nurse($name)
+        {
+            $str_query = "SELECT * from h_account where h_fullname LIKE '%$name'";
+            return $this->query($str_query);
+        }
 
-	function search_nurse($name){
-		$str_query = "SELECT * from h_account where h_fullname LIKE '%$name'";
-		return $this->query($str_query);
-
-	}
-
-	/**
-	*@method boolean get_all() Gets all nurses in the database
-	*@return return bool
-	**/
-
-	function get_all(){
-		$str_query = "SELECT * from h_account";
-		return $this->query($str_query);
-
-	}
-
-
-
-}
+    }
